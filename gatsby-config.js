e715fa87b2479b6e33bb6ca2e83f9c6aad70b81e
zmeyer44 <meyerz@wustl.edu>
2021-10-21 00:14:44 -0500

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,22 +32,20 @@ module.exports = {
         theme_color: `#1273f8`,
         display: `standalone`,
         icon: `src/images/favicons/apple-icon.png`,
-        
       },
     },
     `gatsby-plugin-gatsby-cloud`,
-    `gatsby-plugin-offline`,
     {
       resolve: "gatsby-plugin-firebase",
       options: {
         credentials: {
           apiKey: "AIzaSyDgw5wAn-jsJRxxpMrGmW6VDt0oIL06q34",
-  authDomain: "actionapp-d5e3d.firebaseapp.com",
-  projectId: "actionapp-d5e3d",
-  storageBucket: "actionapp-d5e3d.appspot.com",
-  messagingSenderId: "99724511040",
-  appId: "1:99724511040:web:1f6137f27904b6134e9ca5",
-  measurementId: "G-LEJ9MQ06FD",
+          authDomain: "actionapp-d5e3d.firebaseapp.com",
+          projectId: "actionapp-d5e3d",
+          storageBucket: "actionapp-d5e3d.appspot.com",
+          messagingSenderId: "99724511040",
+          appId: "1:99724511040:web:1f6137f27904b6134e9ca5",
+          measurementId: "G-LEJ9MQ06FD",
         },
       },
     },
