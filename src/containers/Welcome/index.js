c2c59393478ab90e164d808ada3293c4c6d8f405
zmeyer44 <meyerz@wustl.edu>
2021-10-21 01:04:09 -0500

    first commit

--- src/containers/Welcome/index.js
+++ src/containers/Welcome/index.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react"
-import ViralLoops from "viral-loops-react"
 import Fade from "react-reveal/Fade"
 import { Parallax } from "react-scroll-parallax"
 import { WelcomeWrapper, ImagesWrapper } from "./styled.components"
